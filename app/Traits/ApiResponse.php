673518efe626c910291ae9fde57fe9934f077184
G'anisher <git@ganisher.kz>
2023-09-13 17:22:26 +0600

    feat: trait для формирования ответа API

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+class ApiResponse
+{
+    /**
+     * Успешный ответ API
+     *
+     * @param $message
+     * @param array $data
+     * @param int $statusCode
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public static function sendSuccess(string|null $message = null, array $data = [], int $statusCode = 200)
+    {
+        return response()->json([
+            'success'   => true,
+            'message'   => $message ?? __('response.success'),
+            'data'      => $data,
+        ], $statusCode, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Ответ API с ошибкой
+     *
+     * @param $message
+     * @param $data
+     * @param $statusCode
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public static function sendError(string|null $message = null, array $data = [], int $statusCode = 400)
+    {
+        return response()->json([
+            'success'   => false,
+            'message'   => $message ?? __('response.error'),
+            'data'      => $data,
+        ], $statusCode, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+}
