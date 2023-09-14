d183fc01709891904bb1f3a370bc3f137a73bb63
G'anisher <git@ganisher.kz>
2023-09-14 11:46:58 +0600

    style: форматирование кода

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -2,39 +2,53 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\JsonResponse;
+
 trait ApiResponse
 {
     /**
      * Успешный ответ API
      *
-     * @param $message
+     * @param string|null $message
      * @param array $data
      * @param int $statusCode
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public static function sendSuccess(string|null $message = null, array $data = [], int $statusCode = 200)
-    {
+    public static function sendSuccess(
+        string|null $message = null,
+        array $data = [],
+        int $statusCode = 200,
+    ): JsonResponse {
         return response()->json([
-            'success'   => true,
-            'message'   => $message ?? __('response.success'),
-            'data'      => $data,
-        ], $statusCode, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+            'success' => true,
+            'message' => $message ?? __('response.success'),
+            'data' => $data,
+        ],
+            $statusCode,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * Ответ API с ошибкой
      *
-     * @param $message
-     * @param $data
-     * @param $statusCode
-     * @return \Illuminate\Http\JsonResponse
+     * @param string|null $message
+     * @param array $data
+     * @param int $statusCode
+     * @return JsonResponse
      */
-    public static function sendError(string|null $message = null, array $data = [], int $statusCode = 400)
-    {
+    public static function sendError(
+        string|null $message = null,
+        array $data = [],
+        int $statusCode = 400,
+    ): JsonResponse {
         return response()->json([
-            'success'   => false,
-            'message'   => $message ?? __('response.error'),
-            'data'      => $data,
-        ], $statusCode, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+            'success' => false,
+            'message' => $message ?? __('response.error'),
+            'data' => $data,
+        ],
+            $statusCode,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE);
     }
 }
