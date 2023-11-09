511bfab9d80c4218c0e7ed528a6f2220747affc2
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-11-09 14:21:45 +0600

    changed cookie

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -87,7 +87,7 @@ class AuthController extends Controller
         $token = JWTAuth::fromUser($existingUser);
 
         // Set the JWT token as a cookie on the response
-        Cookie::queue('jwt', $token, 60 * 24);
+        Cookie::queue('jwt', $token, 60 * 24, '/', '.ffin.global');
 
         // Redirect to your frontend
         return redirect('http://localhost:5173');
