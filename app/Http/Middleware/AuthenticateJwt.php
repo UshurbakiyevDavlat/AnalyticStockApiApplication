9ce3c8f379e36c5bf8476281521f63660f95d8db
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2023-11-10 16:44:07 +0600

    refactored middleware

--- app/Http/Middleware/AuthenticateJwt.php
+++ app/Http/Middleware/AuthenticateJwt.php
@@ -3,13 +3,14 @@
 namespace App\Http\Middleware;
 
 use App\Enums\AuthStrEnum;
-use App\Enums\StatusCodeEnum;
 use App\Traits\ApiResponse;
 use Closure;
-use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthenticateJwt
@@ -28,42 +29,88 @@ class AuthenticateJwt
         $token = Cookie::get(AuthStrEnum::JWT_NAME->value);
         $link = config('app.url') . '/auth';
 
-        if (!$token) {
-            return self::sendSuccess(
-                'Unauthorized. Token not found.',
-                [
-                    'link' => $link,
-                ],
-            );
-        }
-
         try {
-            $user = JWTAuth::setToken($token)->authenticate();
+            $this->authenticateUser($token);
+        } catch (TokenExpiredException $e) {
+            Log::error($e->getMessage());
 
-            if (!$user) {
-                return self::sendSuccess(
-                    'Unauthorized. Invalid token.',
-                    [
-                        'link' => $link,
-                    ],
-                );
-            }
-
-            return $next($request);
-        } catch (Exception $e) {
-            Log::error(
-                'Error while validating token: '
-                . $e->getMessage(),
-                [
-                    'data' => $e->getTraceAsString(),
-                ],
-            );
-
-            return self::sendError(
-                'Unauthorized. Invalid token.',
-                [],
-                StatusCodeEnum::UNAUTHORIZED->value,
-            );
+            return $this->handleTokenExpired($token);
+        } catch (JWTException $e) {
+            return $this->handleJWTException($e, 'Token not found.', $link);
         }
+
+        return $next($request);
+    }
+
+    /**
+     * Handle JWTException
+     *
+     * @param $message
+     * @param $link
+     * @return JsonResponse
+     */
+    protected function handleUnauthorized($message, $link): JsonResponse
+    {
+        return self::sendSuccess(
+            'Unauthorized. ' . $message,
+            [
+                'link' => $link,
+            ],
+        );
+    }
+
+    /**
+     * Authenticate user
+     *
+     * @throws JWTException
+     */
+    protected function authenticateUser($token): JWTSubject
+    {
+        $user = JWTAuth::setToken($token)->authenticate();
+
+        if (!$user) {
+            throw new JWTException('Invalid token.');
+        }
+
+        return $user;
+    }
+
+    /**
+     * Handle TokenExpiredException
+     *
+     * @param $token
+     * @return JWTSubject|bool|JsonResponse
+     */
+    protected function handleTokenExpired($token): JWTSubject|bool|JsonResponse
+    {
+        $refreshedToken = JWTAuth::refresh($token);
+
+        return JWTAuth::setToken($refreshedToken)->toUser();
+    }
+
+    /**
+     * Handle JWTException
+     *
+     * @param JWTException $e
+     * @param string $message
+     * @param string $link
+     * @return JsonResponse
+     */
+    protected function handleJWTException(
+        JWTException $e,
+        string $message,
+        string $link,
+    ): JsonResponse {
+        Log::error(
+            'Error while validating token: ' . $e->getMessage(),
+            [
+                'data' => $e->getTraceAsString(),
+            ],
+        );
+
+        return $this->handleUnauthorized(
+            $message,
+            $link,
+        );
     }
 }
