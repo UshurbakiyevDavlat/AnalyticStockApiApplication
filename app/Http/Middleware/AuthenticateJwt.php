de096675170678c0100c690c2746c6567bb1ecd7
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-11-14 15:41:13 +0600

    debug

--- app/Http/Middleware/AuthenticateJwt.php
+++ app/Http/Middleware/AuthenticateJwt.php
@@ -28,7 +28,7 @@ class AuthenticateJwt
     {
         // Get the entire request URL
         $referrer = $request->headers->get('Referer');
-        Log::info('Referrer', $referrer);
+        Log::info('Referrer', [$referrer]);
 
         $token = Cookie::get(AuthStrEnum::JWT_NAME->value);
         $link = config('app.url') . '/auth';
