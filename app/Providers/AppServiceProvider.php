5d6e82da3a5f089ab1ff029d4d76750e9f2d5db7
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-11-17 10:39:17 +0600

    added force schema to https if not local env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Contracts\AuthInterface;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme("https");
+        }
     }
 }
