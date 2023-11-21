873790a3184c8bcc5e01c22f68e54fa7ebc7f2eb
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-11-21 14:18:20 +0600

    added User model name and email mutators for encrypt and decrypt

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -80,5 +81,19 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims(): array
     {
         return [];
+    }
+
+    protected function name(): Attribute {
+        return Attribute::make(
+            get: fn (string $value) => decrypt($value),
+            set: fn (string $value) => encrypt($value),
+        );
+    }
+
+    protected function email(): Attribute {
+        return Attribute::make(
+            get: fn (string $value) => decrypt($value),
+            set: fn (string $value) => encrypt($value),
+        );
     }
 }
