37ffe62956c845946a7a651ace7079e6cee953ad
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-12-04 11:33:27 +0600

    added post test

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Enums\StatusCodeEnum;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+test('/get posts', function () {
+    // Assuming you have a user with ID 1
+    $user = User::find(1);
+
+    // Generate a JWT token for the user
+    $token = JWTAuth::fromUser($user);
+
+    // Make a GET request to the /api/v1/posts/ endpoint with the generated token
+    $response = $this->withHeaders([
+        'Authorization' => $token,
+    ])->get('/api/v1/posts/');
+
+    // Assertions
+    $response->assertStatus(StatusCodeEnum::OK->value)
+        ->assertJson([
+            'success' => true,
+            'message' => 'Successfully executed',
+            'data' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'typeId',
+                    'categoryId',
+                    'subcategoriesId',
+                    'createdAt',
+                    'content',
+                    'likes',
+                    'views',
+                    'ticker' => [
+                        'fullName',
+                        'shortName',
+                    ],
+                    'country',
+                    'author' => [
+                        'id',
+                        'name',
+                        'avatar',
+                    ],
+                ],
+            ],
+        ]);
+});
