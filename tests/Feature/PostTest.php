5feba9d4f9dae2b07885083199da0c0813e80aea
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-12-04 11:46:57 +0600

    added post test by id

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -23,3 +23,23 @@ test('/get posts', function () {
             'data' => [],
         ]);
 });
+
+test('/get post by id', function () {
+    // Assuming you have a user with ID 1
+    $user = User::find(1);
+
+    // Generate a JWT token for the user
+    $token = JWTAuth::fromUser($user);
+
+    // Make a GET request to the /api/v1/posts/ endpoint with the generated token
+    $response = $this->withHeaders([
+        'Authorization' => $token,
+    ])->get('/api/v1/posts/1');
+
+    $response->assertStatus(StatusCodeEnum::OK->value)
+        ->assertJson([
+            'success' => true,
+            'message' => 'Successfully executed',
+            'data' => [],
+        ]);
+});
