b10656fc245c085b181354047d5617ecf391038d
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2023-12-14 12:24:50 +0600

    remove debug

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -7,7 +7,6 @@ use App\Enums\AuthStrEnum;
 use App\Enums\EnvStrEnum;
 use App\Models\User;
 use Illuminate\Support\Facades\Cookie;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -102,7 +101,6 @@ class AuthService
         }
 
         $user->avatar_url = Storage::disk('admin')->url($user->avatar_url);
-        Log::info('User created or updated', ['user' => $user]);
 
         return $user;
     }
