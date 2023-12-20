dee858f3d189fe3c2d77967d700399fa8b0b1942
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2023-12-20 15:25:58 +0600

    reformat import

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
-use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OpenApi\Annotations as OA;
