fcc8199c8875415288972125b1fc3bd3058a9ba0
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-01-16 09:28:44 +0600

    added index search records for posts

---
+++ app/Console/Commands/IndexSearchRecords.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class IndexSearchRecords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'meilisearch:index';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Index search records for meilisearch';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->postIndexing();
+    }
+
+    /**
+     *  Index search post-records for Meilisearch.
+     *
+     * @return void
+     */
+    private function postIndexing(): void
+    {
+        $this->info('Indexing search records for Meilisearch...');
+
+        $this->call('php artisan scout:import --model=App\Models\Post');
+
+        $this->info('Indexing completed.');
+    }
+}
