223d46f7f1d62f764aa3d29b96868c21fc636882
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2024-01-19 14:57:55 +0600

    added cache clear command with logs, and reset caching in categories endpoints

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('meilisearch:index')->everyTenMinutes();
+        $schedule->command('app:cache-clearing')->everyThirtyMinutes();
     }
 
     /**
