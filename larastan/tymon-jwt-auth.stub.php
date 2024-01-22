82b8c602ee980cbeef1a12c637722ad50bb78f5b
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-01-22 17:08:57 +0600

    debug

--- larastan/tymon-jwt-auth.stub.php
+++ larastan/tymon-jwt-auth.stub.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 namespace Tymon\JWTAuth;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
-
 /**
  * @method static toUser()
  * @method static authenticate()
- * @method static Payload setToken(JWT|array|string|null $token)
- * @method static Payload fromUser(JWTSubject $user)
- * @method static Payload refresh()
  * @see JWT
  */
 class JWTAuth
