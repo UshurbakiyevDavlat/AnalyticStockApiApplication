3d1f1366ffce33180d105f725f5b01dca9ed5bfa
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-01-22 16:56:04 +0600

    updated neon config

--- larastan/tymon-jwt-auth.stub.php
+++ larastan/tymon-jwt-auth.stub.php
@@ -2,17 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Tymon\JWTAuth\Facades;
+namespace Tymon\JWTAuth;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Tymon\JWTAuth\JWT;
-use Tymon\JWTAuth\Payload;
 
 /**
- * @method static Payload setToken(JWT|array|string|null $token)
- * @method static Payload fromUser(JWTSubject $user)
- * @method static Payload toUser(JWT|array|string|null $token)
- * @method static Payload refresh()
+ * @method static toUser()
+ * @method static authenticate()
  * @see JWT
  */
 class JWTAuth
