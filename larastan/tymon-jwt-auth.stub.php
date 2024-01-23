345ae24881908b89608060248e4f3a1fac19ed8e
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-01-23 09:52:21 +0600

    fixed jwt auth stub

--- larastan/tymon-jwt-auth.stub.php
+++ larastan/tymon-jwt-auth.stub.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
-namespace Tymon\JWTAuth;
+namespace Tymon\JWTAuth\Facades;
 
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\JWT;
+use Tymon\JWTAuth\Payload;
 
 /**
- * @method static Payload setToken(JWT|array|string|null $token)
- * @method static Payload fromUser(JWTSubject $user)
- * @method static Payload refresh()
+ * @method static mixed setToken(JWT|array|string|null $token)
+ * @method static mixed fromUser(JWTSubject $user)
+ * @method static mixed refresh()
  * @method static toUser()
  * @method static authenticate()
  *
