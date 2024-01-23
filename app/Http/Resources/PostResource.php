4ad996f8f1dda131582c3f16e58bcbe6c95ef609
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-01-23 14:33:24 +0600

    added null safe for horizondataset nullable case

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -123,10 +123,10 @@ class PostResource extends JsonResource
             'sector' => $this->horizonDataset?->sector?->title,
             'postType' => $this->postType?->title,
             'ticker' => [
-                'fullName' => $this->horizonDataset->ticker?->full_name,
-                'shortName' => $this->horizonDataset->ticker?->short_name,
+                'fullName' => $this->horizonDataset?->ticker?->full_name,
+                'shortName' => $this->horizonDataset?->ticker?->short_name,
             ],
-            'isin' => $this->horizonDataset->isin?->code,
+            'isin' => $this->horizonDataset?->isin?->code,
             'potential' => $this->horizonDataset?->potential,
             'country' => $this->horizonDataset?->country?->title,
             'author' => [
