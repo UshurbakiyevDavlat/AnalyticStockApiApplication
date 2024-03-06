fb52b51c56bd4eaa9f91faebd7c754b398a03c6d
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-03-06 16:47:04 +0500

    added additional controllers endpoints unit test

---
+++ tests/Unit/DataControllingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+
+test('ecosystem controller test', function () {
+    $user = User::find(1);
+    $token = JWTAuth::fromUser($user);
+
+    $response = $this->withHeaders([
+        'Authorization' => $token,
+    ])
+        ->get(route('getEcosystem'));
+
+    expect($response->getStatusCode())
+        ->toBe(200)
+        ->and($response->getOriginalContent())
+        ->toHaveKey('data');
+});
+
+test('file controller test', function () {
+    $user = User::find(1);
+    $token = JWTAuth::fromUser($user);
+
+    $response = $this->withHeaders([
+        'Authorization' => $token,
+    ])
+        ->get(route('getPostFiles',1));
+
+    expect($response->getStatusCode())
+        ->toBe(200)
+        ->and($response->getOriginalContent())
+        ->toHaveKey('data');
+});
