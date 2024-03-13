a1da1099d2d17c49b61ca0dcc285e6447032fb7b
Davlatbek Ushurbakiyev <davlatbekushurbakiyev@Davlatbeks-MacBook-Pro.local>
2024-03-13 11:52:17 +0500

    pint fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,6 @@ namespace App\Models;
 use App\Enums\StatusActivityEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OpenApi\Annotations as OA;
